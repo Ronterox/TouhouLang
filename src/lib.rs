e3c7c9538e9d5fe8c7ef9e764069e985408e8572
Ricardo <ricarditomontserrat@gmail.com>
2024-09-07 22:49:39 +0200

    Rewrote touhoulang code into tokenizer, parsing instead of just parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,411 +1,139 @@
-mod macros;
-
-use macros::concat;
-use std::collections::HashMap;
-
-const NUM_KEYWORDS: [&str; 29] = [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-    "ten",
-    "eleven",
-    "twelve",
-    "thirteen",
-    "fourteen",
-    "fifteen",
-    "sixteen",
-    "seventeen",
-    "eighteen",
-    "nineteen",
-    "twenty",
-    "thirty",
-    "forty",
-    "fifty",
-    "sixty",
-    "seventy",
-    "eighty",
-    "ninety",
-    "hundred",
-];
-
-const CONTROL_KEYWORDS: [&str; 12] = [
-    "if", "else", "is", "it", "and", "with", "when", "to", "can", "in", "out", "on",
-];
-const POSITION_KEYWORDS: [&str; 6] = ["up", "down", "left", "right", "top", "bottom"];
-const POSSESIVE_KEYWORDS: [&str; 3] = ["its", "has", "of"];
-const OBJECT_KEYWORDS: [&str; 2] = ["enemy", "player"];
-
-const KEYWORDS: [&str; 23] = concat_array!(
-    CONTROL_KEYWORDS,
-    POSITION_KEYWORDS,
-    POSSESIVE_KEYWORDS,
-    OBJECT_KEYWORDS
-);
-
-const GLOBAL_METHODS: [&str; 1] = ["display"];
-
-#[derive(Debug, Clone)]
+#[derive(Debug, PartialEq)]
 enum Token {
-    Method(String),
-    Property(String),
-    Variable(String),
+    Preposition(String),
+    Identifier(String),
     Keyword(String),
-    Number(String),
-    List(String),
-    Text(String),
+    Integer(i32),
+    Float(f32),
 }
 
-fn parse_num_word(word: &str) -> String {
-    match word {
-        "zero" => "0",
-        "one" => "1",
-        "two" => "2",
-        "three" => "3",
-        "four" => "4",
-        "five" => "5",
-        "six" => "6",
-        "seven" => "7",
-        "eight" => "8",
-        "nine" => "9",
-        "ten" => "10",
-        "eleven" => "11",
-        "twelve" => "12",
-        "thirteen" => "13",
-        "fourteen" => "14",
-        "fifteen" => "15",
-        "sixteen" => "16",
-        "seventeen" => "17",
-        "eighteen" => "18",
-        "nineteen" => "19",
-        "twenty" => "20",
-        "thirty" => "30",
-        "forty" => "40",
-        "fifty" => "50",
-        "sixty" => "60",
-        "seventy" => "70",
-        "eighty" => "80",
-        "ninety" => "90",
-        "hundred" => "100",
-        _ => word,
-    }
-    .to_string()
-}
+const PREPOSITIONS: [&str; 4] = ["the", "a", "an", "is"];
 
-fn tokenize_word(word_chars: &mut Vec<char>, context: char, tokens: &mut Vec<Token>) -> char {
-    let word: String = word_chars.iter().collect();
-    word_chars.clear();
+#[allow(dead_code)]
+fn tokenize(text: &str, keywords: &[&str]) -> Vec<Token> {
+    let mut tokens = vec![];
+    let mut chars = text
+        .trim()
+        .chars()
+        .skip_while(|c| c.is_whitespace())
+        .peekable();
 
-    let word = match word.trim() {
-        "the" | "then" | "a" | "an" | "at" => "",
-        "are" => "is",
-        _ => word.trim(),
-    };
+    while let Some(c) = chars.peek() {
+        let token = match c {
+            'a'..='z' | 'A'..='Z' | '_' => {
+                let condition = |c: &char| c.is_alphanumeric() || *c == '_';
+                let word = String::from_iter(chars.by_ref().take_while(condition));
 
-    if word.is_empty() {
-        return context;
-    }
-
-    let token: Token;
-    if context == ')' {
-        token = Token::List(word.to_string());
-    } else if context == '"' {
-        token = Token::Text(word.to_string());
-    } else if context == 's' {
-        if word == "'s" {
-            return context;
-        }
-        token = Token::Property(word.to_string());
-    } else if GLOBAL_METHODS.contains(&word) {
-        token = Token::Method(word.to_string());
-    } else if KEYWORDS.contains(&word) {
-        token = Token::Keyword(word.to_string());
-        if word == "of" {
-            if let Some(Token::Variable(varname)) = tokens.last() {
-                *tokens.last_mut().unwrap() = Token::Property(varname.to_owned());
-            }
-        }
-    } else if word.chars().all(char::is_numeric) || NUM_KEYWORDS.contains(&word) {
-        token = Token::Number(parse_num_word(&word));
-    } else if word.ends_with('s') {
-        token = Token::Method(word.to_string());
-    } else {
-        token = Token::Variable(word.to_string());
-    }
-
-    tokens.push(token);
-    char::default()
-}
-
-fn tokenize_line(line: &str, tokens: &mut Vec<Token>) {
-    let mut word_chars = Vec::<char>::new();
-    let mut context = char::default();
-    let mut chars = line.trim().chars().enumerate();
-
-    while let Some((i, c)) = chars.next() {
-        if c.is_whitespace() && !context.is_ascii_punctuation() || context == c {
-            word_chars.push(c);
-            context = tokenize_word(&mut word_chars, context, tokens);
-            continue;
-        } else if c.is_ascii_punctuation() && !context.is_ascii_punctuation() {
-            if !word_chars.is_empty() {
-                context = tokenize_word(&mut word_chars, context, tokens);
-            }
-
-            if c == ',' {
-                tokenize_line(line.split_at(i + 1).1, tokens);
-                break;
-            }
-
-            context = match c {
-                '(' => ')',
-                '"' => '"',
-                '\'' => 's',
-                _ => char::default(),
-            };
-        }
-        word_chars.push(c);
-    }
-
-    if !word_chars.is_empty() {
-        tokenize_word(&mut word_chars, context, tokens);
-    }
-}
-
-fn tokenize_text(text: &str) -> Vec<Token> {
-    let mut tokens = Vec::new();
-    text.split(".\n")
-        .filter(|line| line.len() > 0 && line.trim().chars().next().unwrap() != '#')
-        .for_each(|line| {
-            tokenize_line(line, &mut tokens);
-        });
-    tokens
-}
-
-fn parse_word_value(token: Token, variables: &HashMap<String, String>) -> String {
-    match token {
-        Token::Variable(varname) => variables.get(&varname).unwrap_or(&varname).to_owned(),
-        Token::Keyword(varname) => varname,
-        Token::Number(num) => num,
-        Token::Text(text) => text,
-        Token::List(list) => list,
-        _ => "".to_owned(),
-    }
-}
-
-fn next_token(tokens: &mut impl Iterator<Item = Token>, error: &str) -> Token {
-    tokens.next().expect(format!("expected {error}").as_str())
-}
-
-fn update_variable(
-    name: String,
-    error: &str,
-    variables: &mut HashMap<String, String>,
-    tokens: &mut impl Iterator<Item = Token>,
-) {
-    let token = next_token(tokens, &error);
-    let value = parse_word_value(token, variables);
-
-    if OBJECT_KEYWORDS.contains(&value.as_str()) {
-        variables.insert(value, name);
-    } else {
-        variables.insert(name, value);
-    }
-}
-
-// TODO: Generic parsing for actual rust types, so it outputs them out
-pub fn parse_text(text: &str) -> (HashMap<String, String>, Vec<(String, String)>) {
-    let tokens = tokenize_text(text);
-
-    let mut tokens = tokens.into_iter();
-    let mut actions: Vec<(String, String)> = Vec::new();
-    let mut variables: HashMap<String, String> = HashMap::new();
-
-    while let Some(token) = tokens.next() {
-        match token {
-            Token::Variable(name) => match next_token(&mut tokens, "token after variable") {
-                Token::Keyword(keyword) if keyword == "is" => update_variable(
-                    name.to_owned(),
-                    "value after 'is'",
-                    &mut variables,
-                    &mut tokens,
-                ),
-                Token::Property(property) => match next_token(&mut tokens, "keyword 'is'") {
-                    Token::Keyword(keyword) if keyword == "is" => update_variable(
-                        format!("{name}.{property}"),
-                        "value after 'is'",
-                        &mut variables,
-                        &mut tokens,
-                    ),
-                    Token::Variable(varname) => {
-                        let mut full_name = format!("{name}.{property}.{varname}");
-
-                        while let Some(token) = tokens.next() {
-                            match token {
-                                Token::Variable(varname) => {
-                                    full_name = format!("{full_name}.{varname}");
-                                }
-                                Token::Keyword(keyword) if keyword == "is" => {
-                                    update_variable(
-                                        full_name.to_owned(),
-                                        "value after 'is'",
-                                        &mut variables,
-                                        &mut tokens,
-                                    );
-                                    break;
-                                }
-                                _ => {}
-                            }
-                        }
-                    }
-                    _ => {}
-                },
-                Token::Method(method) => {
-                    actions.push((name.to_owned(), method.to_owned()));
-                }
-                _ => {}
-            },
-            Token::Property(property) => match next_token(&mut tokens, "keyword 'of'") {
-                Token::Keyword(keyword) if keyword == "of" => {
-                    match next_token(&mut tokens, "variable after 'of'") {
-                        Token::Variable(name) => match next_token(&mut tokens, "keyword 'is'") {
-                            Token::Keyword(keyword) if keyword == "is" => update_variable(
-                                format!("{name}.{property}"),
-                                "value after 'is'",
-                                &mut variables,
-                                &mut tokens,
-                            ),
-                            _ => {}
-                        },
-                        _ => {}
-                    }
-                }
-                _ => {}
-            },
-            Token::Keyword(name) if OBJECT_KEYWORDS.contains(&name.as_str()) => {
-                match next_token(&mut tokens, "keyword 'is'") {
-                    Token::Keyword(keyword) if keyword == "is" => {
-                        let token = next_token(&mut tokens, "value after 'is'");
-                        let value = parse_word_value(token, &variables);
-                        variables.insert(name, value);
-                    }
-                    _ => {}
+                if keywords.contains(&word.as_str()) {
+                    Token::Keyword(word)
+                } else if PREPOSITIONS.contains(&word.as_str()) {
+                    Token::Preposition(word)
+                } else {
+                    Token::Identifier(word)
                 }
             }
-            _ => (),
-        }
+            '0'..='9' => {
+                let condition = |c: &char| c.is_digit(10) || *c == '.';
+                let number = String::from_iter(chars.by_ref().take_while(condition));
+                if number.contains('.') {
+                    Token::Float(number.parse().unwrap())
+                } else {
+                    Token::Integer(number.parse().unwrap())
+                }
+            }
+            _ => break,
+        };
+        tokens.push(token);
     }
 
-    (variables, actions)
+    return tokens;
 }
 
 #[cfg(test)]
-mod test {
-    use crate::parse_text;
-    use std::collections::HashMap;
+mod test_tokenizer {
+    use crate::{tokenize, Token};
 
-    fn expect<const N: usize>(text: &str, result: [(&str, &str); N]) {
-        let (mut vars, _) = parse_text(text);
-        let vars: HashMap<&str, &str> = vars
-            .iter_mut()
-            .map(|(a, b)| (a.as_str(), b.as_str()))
-            .collect();
-
-        dbg!(&vars);
-        assert_eq!(vars, HashMap::from(result));
+    struct ABC {
+        a: String,
+        b: bool,
+        c: i32,
     }
 
-    fn expect_fail(text: &str) {
-        assert!(std::panic::catch_unwind(|| parse_text(text)).is_err());
+    impl Default for ABC {
+        fn default() -> Self {
+            Self {
+                a: "A".to_string(),
+                b: true,
+                c: 0,
+            }
+        }
     }
 
-    #[test]
-    fn test_keyword_is() {
-        expect("reimu is 18", [("reimu", "18")]);
-        expect("reimu is 18, reimu is reimu", [("reimu", "18")]);
+    const KEYWORDS: [&str; 2] = ["reimu", "marisa"];
 
-        expect(
-            "marissa is 18, reimu is marissa",
-            [("reimu", "18"), ("marissa", "18")],
-        );
-
-        expect(
-            "reimu is 18 and the person is reimu",
-            [("reimu", "18"), ("person", "18")],
-        );
-
-        expect("chan is twelve", [("chan", "12")]);
-
-        let text = "\"A donburin\"";
-        expect(format!("reimu is {text}").as_str(), [("reimu", text)]);
+    fn expect<const N: usize>(text: &str, tokens: [Token; N]) {
+        assert_eq!(tokenize(text, &KEYWORDS), tokens);
     }
 
     #[test]
-    fn test_properties() {
-        let text = "\"A mage\"";
-        expect(
-            format!("marissa is {text}, reimu's donburin is a marissa").as_str(),
-            [("marissa", text), ("reimu.donburin", text)],
-        );
-
-        expect("reimu's donburin is 10", [("reimu.donburin", "10")]);
-        expect(
-            "the person's pattern is (left right right left)",
-            [("person.pattern", "(left right right left)")],
-        );
-
-        expect(
-            "the health of marissa is a hundred",
-            [("marissa.health", "100")],
-        );
-
-        expect("marissa's bullets are 20", [("marissa.bullets", "20")]);
-
-        expect_fail("marissa has 20 health");
-        expect_fail("marissa's health is 20, marissa has 1 health");
-
-        expect("reimu's bullet speed is 20", [("reimu.bullet.speed", "20")]);
-        expect(
-            "reimu's bullet minecraft speed is \"slow\"",
-            [("reimu.bullet.minecraft.speed", "\"slow\"")],
-        );
-
-        // expect_fail("the bullet speed of reimu is 10");
+    fn empty_is_empty_list() {
+        expect("", []);
     }
 
     #[test]
-    fn test_object_keywords() {
-        expect(
-            "the player is reimu, the enemy is marissa",
-            [("player", "reimu"), ("enemy", "marissa")],
-        );
-
-        expect(
-            "reimu is the player, and marissa is the enemy",
-            [("player", "reimu"), ("enemy", "marissa")],
-        );
+    fn recognizes_identifiers() {
+        expect("abc", [Token::Identifier("abc".to_string())]);
     }
 
     #[test]
-    fn multiple_commands() {
+    fn ignores_whitespace() {
+        expect("    abc    ", [Token::Identifier("abc".to_string())]);
+    }
+
+    #[test]
+    fn recognizes_numbers() {
+        expect("123", [Token::Integer(123)]);
+        expect("1.23", [Token::Float(1.23)]);
+    }
+
+    #[test]
+    fn recognizes_keywords() {
         expect(
-            "rei's bullet is 10, rei's bullet speed is 20, rei's name is \"rei\"",
+            "reimu marisa",
             [
-                ("rei.bullet", "10"),
-                ("rei.bullet.speed", "20"),
-                ("rei.name", "\"rei\""),
+                Token::Keyword("reimu".to_string()),
+                Token::Keyword("marisa".to_string()),
             ],
         );
     }
 
     #[test]
-    fn comments() {
-        expect("# rei's bullet is 10.\nrei is 15.\n", [("rei", "15")]);
+    fn recognizes_prepositions() {
+        expect(
+            "the a an",
+            [
+                Token::Preposition("the".to_string()),
+                Token::Preposition("a".to_string()),
+                Token::Preposition("an".to_string()),
+            ],
+        );
+    }
+
+    #[test]
+    fn multiple_tokens() {
+        expect(
+            "abc 123",
+            [Token::Identifier("abc".to_string()), Token::Integer(123)],
+        );
+
+        expect(
+            "reimu age is 17",
+            [
+                Token::Keyword("reimu".to_string()),
+                Token::Identifier("age".to_string()),
+                Token::Preposition("is".to_string()),
+                Token::Integer(17),
+            ],
+        );
     }
 }
